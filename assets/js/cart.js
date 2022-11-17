4bc0c602cc6a80de2545f5e105d6b9efbfe0dcea
Gennerentola <gennaro.notaro1@gmail.com>
2022-11-17 21:31:22 +0100

    fix ulteriori carrello

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -7,14 +7,14 @@ var barraPagamento = document.getElementById("barraPagamento");
 var costoTotale = document.getElementById("costoTotale");
 var cartReview = document.getElementById("riepilogo");
 var numArticoli = document.getElementById("numArticoli");
-var idUtente = sessionStorage.getItem('id');
+var idUtente = sessionStorage.getItem("id");
+var recapAddress = document.getElementById("indirizzoConsegna");
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     visualizzaCarrello();
     svuotaBtn.style.display = "none";
-    barraPagamento.classList.remove("d-lg-block");
     barraPagamento.classList.add("d-none");
 }
 
@@ -25,7 +25,7 @@ function visualizzaCarrello() {
         })
         .then((data) => {
             arrayCart = data;
-            if (arrayCart.cart.length > 0) {
+            if (arrayCart.cart.length > 0 && arrayCart.cart.length) {
                 svuotaBtn.style.display = "block";
                 barraPagamento.classList.remove("d-none");
                 emptyCartPlaceholder.style.display = "none";
@@ -73,12 +73,14 @@ function visualizzaCarrello() {
                     dt.innerHTML += arrayCart.cart[i].title;
                     cartReview.appendChild(li);
                     li.innerHTML += arrayCart.cart[i].title;
+                    recapAddress.innerHTML = `Indirizzo di consegna: ${arrayCart.indirizzo} ${arrayCart.citta} ${arrayCart.cap}`;
                 };
                 numArticoli.innerHTML += `<i class="fas fa-boxes"></i> Articoli nel carrello: ${arrayCart.cart.length}`;
                 sommaPrezzi();
             } else {
                 barraPagamento.classList.remove("d-lg-block");
                 barraPagamento.classList.add("d-none");
+                return;
             }
         });
 }
@@ -144,16 +146,18 @@ var paySection = document.getElementById("paySection");
 var widthLg = window.matchMedia("(min-width: 992px)");
 
 if (widthLg.matches) {
-    barraPagamento.classList.remove("fixed-bottom");
+    barraPagamento.classList.remove("fixed-bottom", "d-flex");
     barraPagamento.classList.add("rounded", "border");
     paySection.classList.add("position-fixed", "end-0");
+    barraPagamento.classList.add("d-block");
 }
 
 window.addEventListener("resize", () => {
     if (widthLg.matches) {
-        barraPagamento.classList.remove("fixed-bottom");
+        barraPagamento.classList.remove("fixed-bottom", "d-flex");
         barraPagamento.classList.add("rounded", "border");
-        paySection.classList.add("position-fixed", "end-0");
+        paySection.classList.add("position-fixed", "end-0");   
+        barraPagamento.classList.add("d-block");
     }
 })
 
@@ -161,9 +165,10 @@ var fixWidth = window.matchMedia("(max-width: 991px)");
 
 window.addEventListener("resize", () => {
     if (fixWidth.matches) {
-        barraPagamento.classList.add("fixed-bottom");
+        barraPagamento.classList.add("fixed-bottom", "d-flex");
         barraPagamento.classList.remove("rounded", "border");
-        paySection.classList.remove("position-fixed", "end-0");
+        paySection.classList.remove("position-fixed", "end-0");    
+        barraPagamento.classList.remove("d-block");
     }
 })
 
